99a6f09d2f2f61611937eeb1168995bc29bdedeb
wangzilong1997 <1321787581@qq.com>
2021-07-18 11:05:43 +0800

    直接build

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function(app) {
   app.use('/huyapenta', createProxyMiddleware({
-    target: 'http://82.156.183.85:8080/',
+    target: 'http://82.156.183.85:80/',
     changeOrigin: true,
   }))
 }
