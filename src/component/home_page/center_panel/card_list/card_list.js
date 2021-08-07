c4642f55bc664bf7d0e24148d0866c2d79d69c9f
wangzilong1997 <1321787581@qq.com>
2021-08-08 00:18:46 +0800

    修改页面

--- src/component/home_page/center_panel/card_list/card_list.js
+++ src/component/home_page/center_panel/card_list/card_list.js
@@ -26,6 +26,7 @@ class Card_list extends React.Component {
         return(
             <div className="card_container" onClick={this.cardClick}>
                 <div className="card_id">{this.props.data.pentaid}</div>
+                <div className="card_views">{this.props.data.views}</div>
                 <div className="card_img"> 
                     <a href={this.props.data.url} target="_blank" rel="noopener noreferrer">
                         <img src={this.props.data.imgurl} alt="图片路径" width="290px" height="200px"/>
