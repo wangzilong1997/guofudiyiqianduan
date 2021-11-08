57020da74909ca503fd510fb9b8a4c6452caadfc
wangzilong1997 <1321787581@qq.com>
2021-11-08 18:21:38 +0800

    :ship: 改变为hash router

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import './App.css'
 import Login from './component/login/index'
 import Layout from './layout/index'
 import React from 'react'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {HashRouter, Route, Switch} from 'react-router-dom'
 
 import ProctectedRoute from './utils/proctected.router'
 function App() {
   return (
     <>
     <div className='app'>
-      <BrowserRouter>
+      <HashRouter>
         <ProctectedRoute exact path="/" component={Layout} />
         {/* <Route exact path='/' >
           <Header></Header>
@@ -19,7 +19,7 @@ function App() {
         </Route> */}
         <Route exact path='/login' component={Login} />
           
-      </BrowserRouter>
+      </HashRouter>
     </div>
     </>
   );
